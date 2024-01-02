1b638fc93343bdd71128cfc4f5dd80091546424e
Allejandro <allejandro.empresarial@gmail.com>
2024-01-02 15:26:40 -0300

    Breaking down a larger function into smaller functions

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,20 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """Responds to key presses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Responds to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Responds to keyboard and mouse input events."""
     for event in pygame.event.get():
@@ -8,17 +22,11 @@ def check_events(ship):
             sys.exit()
         
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-    
+            check_keyup_events(event, ship)
+  
 def update_screen(oea_settings, screen, ship):
     """Update the images on the screen and changes to the new screen"""
     screen.fill(oea_settings.bg_color)
