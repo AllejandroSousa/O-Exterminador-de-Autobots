bf57165a4e6afbf9935a3c26e4615ed04f0856ef
Allejandro <allejandro.empresarial@gmail.com>
2024-01-03 08:29:33 -0300

    Developing the module bullet

---
+++ bullet.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Bullet(Sprite):
+    """"A class that manages projectiles fired by the spaceship."""
+
+    def __init__(self, oea_settings, screen, ship):
+        """Create an object to the projectile in the spaceship current position."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+
+        self.rect = pygame.Rect(0, 0, oea_settings.bullet_width, oea_settings.bullet_height)
+        self.rect.centerx = ship.rect.centerx
+        self.rect.top = ship.rect.top
+
+        self.y = float(self.rect.y)
+
+        self.color = oea_settings.bullet_color
+        self.speed_factor = oea_settings.bullet_speed_factor
+
+    def update(self):
+        """Move up the projectile on the screen."""
+        self.y -= self.speed_factor
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the projectile on the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
