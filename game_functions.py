84e86af611b8b66340776ee5d71bf2c9748a984e
Allejandro <allejandro.empresarial@gmail.com>
2024-01-03 08:31:00 -0300

    Implementing the bullets in the game functions

--- game_functions.py
+++ game_functions.py
@@ -1,12 +1,21 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def fire_bullet(oea_settings, screen, ship, bullets):
+    """Fire a projectile if the limit has not been reached."""
+    if len(bullets) < oea_settings.bullets_allowed:
+        new_bullet = Bullet(oea_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keydown_events(event, oea_settings, screen, ship, bullets):
     """Responds to key presses."""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(oea_settings, screen, ship, bullets)
 
 def check_keyup_events(event, ship):
     """Responds to key releases."""
@@ -15,21 +24,32 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(oea_settings, screen, ship, bullets):
     """Responds to keyboard and mouse input events."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, oea_settings, screen, ship, bullets)
         
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
   
-def update_screen(oea_settings, screen, ship):
-    """Update the images on the screen and changes to the new screen"""
+def update_screen(oea_settings, screen, ship, bullets):
+    """Update the images on the screen and changes to the new screen."""
     screen.fill(oea_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """Update the projectile's position and remove the old projectiles."""
+    bullets.update()
+
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+    
