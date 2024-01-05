b68b4cda558f917721cc8616b32d0f4ece952518
Allejandro <allejandro.empresarial@gmail.com>
2024-01-05 15:10:27 -0300

    Inserting the changes on the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
@@ -12,6 +13,7 @@ def run_game():
     screen = pygame.display.set_mode((oea_settings.screen_width, oea_settings.screen_height))
     pygame.display.set_caption("O Exterminador de Autobots")
 
+    play_button = Button(oea_settings, screen, "Play")
     stats = GameStats(oea_settings)
 
     ship = Ship(oea_settings ,screen)
@@ -22,13 +24,13 @@ def run_game():
 
     #Initialize the principal loop of the game
     while True:
-        gf.check_events(oea_settings, screen, ship, bullets)
+        gf.check_events(oea_settings, screen, stats, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(oea_settings, screen, ship, aliens, bullets)
             gf.update_aliens(oea_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(oea_settings, screen, ship, aliens, bullets)
+        gf.update_screen(oea_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
             
