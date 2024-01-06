474d3bc61dc9c3ff7caaf1b482d739aae15e54b8
Allejandro <allejandro.empresarial@gmail.com>
2024-01-06 15:44:29 -0300

    Adding a score

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ class Settings():
         """Initialize the game settings."""
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
         self.ship_limit = 3
 
         self.bullet_width = 3
@@ -13,10 +13,12 @@ class Settings():
         self.bullet_color = 255, 0, 0
         self.bullets_allowed = 3
 
-        self.fleet_drop_speed = 10
-        
+        self.fleet_drop_speed = 10        
         self.speed_up_scale = 1.1
+        self.score_scale = 1.5
         
+        self.initialize_dynamic_settings()
+
     def initialize_dynamic_settings(self):
         """Initialize the settings that change during the game."""
         self.ship_speed_factor = 1.5
@@ -26,8 +28,12 @@ class Settings():
         #fleet_direction equals to 1 represents the right; -1 represents the left
         self.fleet_direction = 1
 
+        self.alien_points = 50
+
     def increase_speed(self):
         """Increase the speed settings."""
         self.ship_speed_factor *= self.speed_up_scale
         self.bullet_speed_factor *= self.speed_up_scale
         self.alien_speed_factor *= self.speed_up_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
